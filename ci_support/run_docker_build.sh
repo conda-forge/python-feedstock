6fda08415f15d564a4c14fd133427826a2f546c1
Jonathan Helmus <jjhelmus@gmail.com>
2016-07-12 16:20:59 -0500

    Upload to prerelease channel

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -49,5 +49,6 @@ conda info
     export CONDA_PY=36
     set +x
     conda build /recipe_root --quiet || exit 1
-    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
+    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root
+    conda-forge --channel=prerelease || exit 1
 EOF

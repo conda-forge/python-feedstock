90f96d290ad5d6a7a85969e2767fc9b7419160a6
Jonathan Helmus <jjhelmus@gmail.com>
2016-07-13 11:42:24 -0500

    Rerender recipe

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -14,14 +14,12 @@ config=$(cat <<CONDARC
 
 channels:
  - conda-forge
-
  - defaults # As we need conda-build
 
 conda-build:
  root-dir: /feedstock_root/build_artefacts
 
 show_channel_urls: true
-add_pip_as_python_dependency: false
 
 CONDARC
 )
@@ -40,9 +38,8 @@ echo "$config" > ~/.condarc
 # A lock sometimes occurs with incomplete builds. The lock file is stored in build_artefacts.
 conda clean --lock
 
-conda update --yes --all
-conda install --yes conda-build
-conda info
+conda install --yes --quiet conda-forge-build-setup
+source run_conda_forge_build_setup
 
 # Embarking on 1 case(s).
     set -x

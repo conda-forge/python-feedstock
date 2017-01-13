d6a92b2c52fe9ebc9ab8772a5853fd0631767aef
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2017-01-13 15:46:52 -0200

    Version of python interpreter fixed

--- recipe/build.sh
+++ recipe/build.sh
@@ -25,5 +25,5 @@ fi
 
 make
 make install
-ln -s $PREFIX/bin/python3.6 $PREFIX/bin/python
-ln -s $PREFIX/bin/pydoc3.6 $PREFIX/bin/pydoc
+ln -s $PREFIX/bin/python3.5 $PREFIX/bin/python
+ln -s $PREFIX/bin/pydoc3.5 $PREFIX/bin/pydoc

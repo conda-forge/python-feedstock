ba361b5f2793d8ace8f9482c178821d38b8bad96
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-12-09 12:03:50 -0500

    Refresh pinnings

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -99,7 +99,7 @@ if not (armv6l or armv7l or ppc64le or osx105):
     import _tkinter
     print('TK_VERSION: %s' % _tkinter.TK_VERSION)
     print('TCL_VERSION: %s' % _tkinter.TCL_VERSION)
-    TCLTK_VER = '8.6' if sys.platform == 'win32' else '8.5'
+    TCLTK_VER = '8.6' if sys.platform == 'win32' else '8.6'
     assert _tkinter.TK_VERSION == _tkinter.TCL_VERSION == TCLTK_VER
 
 import ssl

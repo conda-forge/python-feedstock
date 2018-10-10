6ac8788d42d854c6eb7fe13c5fb1012685c6444a
Jonathan Helmus <jjhelmus@gmail.com>
2018-10-10 16:51:27 -0500

    split builds based on compilers used
    Use a different set of patches and build scripts depending on what compilers
    are used.

---
+++ recipe/build.sh
@@ -0,0 +1,8 @@
+
+if [[ ${c_compiler} =~ .*toolchain.* ]]; then
+    cp ${RECIPE_DIR}/toolchain_build.sh do_build.sh
+else
+    cp ${RECIPE_DIR}/gcc_build.sh do_build.sh
+fi
+chmod +x do_build.sh
+./do_build.sh

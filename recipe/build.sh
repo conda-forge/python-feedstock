875612d88ef30fb0303a39c0ef257059ae306dae
Marius van Niekerk <marius.v.niekerk@gmail.com>
2019-02-17 18:17:41 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,5 +9,5 @@ chmod +x do_build.sh
 ./do_build.sh
 
 # There are some strange distutils files around. Delete them
-rm -rf ${PREFIX}lib/python${VER}/distutils/command/*.exe
+rm -rf ${PREFIX}/lib/python${VER}/distutils/command/*.exe
 

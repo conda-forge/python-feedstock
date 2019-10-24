b6bebecdd8b9dda05386d188c82bf37347b76074
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2019-10-24 11:44:31 +0000

    MNT: Re-rendered with conda-build 3.18.10, conda-smithy 3.6.0, and conda-forge-pinning 2019.10.11

--- .scripts/run_docker_build.sh
+++ .scripts/run_docker_build.sh
@@ -39,7 +39,7 @@ if [ -z "$CONFIG" ]; then
 fi
 
 if [ -z "${DOCKER_IMAGE}" ]; then
-    SHYAML_INSTALLED="$(shyaml --version || echo NO)"
+    SHYAML_INSTALLED="$(shyaml -h || echo NO)"
     if [ "${SHYAML_INSTALLED}" == "NO" ]; then
         echo "WARNING: DOCKER_IMAGE variable not set and shyaml not installed. Falling back to condaforge/linux-anvil-comp7"
         DOCKER_IMAGE="condaforge/linux-anvil-comp7"
@@ -64,6 +64,8 @@ docker run ${DOCKER_RUN_ARGS} \
            -e BINSTAR_TOKEN \
            -e HOST_USER_ID \
            -e UPLOAD_PACKAGES \
+           -e GIT_BRANCH \
+           -e UPLOAD_ON_BRANCH \
            -e CI \
            $DOCKER_IMAGE \
            bash \

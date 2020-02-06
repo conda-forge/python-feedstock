53534554efc9d3e4f92ff216697f82011f2d4910
Ray Donnelly <mingw.android@gmail.com>
2020-02-06 23:38:08 +0100

    Fixes for debug builds, remove posix build dep

--- build-locally-debug.bat
+++ build-locally-debug.bat
@@ -1,12 +1,17 @@
+set PF=win-32
+set PF=win-64
+
 set PY_INTERP_DEBUG=yes
 call conda activate
 del /s /q %CONDA_PREFIX%\conda-bld\python
+del /s /q %CONDA_PREFIX%\conda-bld\python-3.8-dbg-%PF%
 mkdir %CONDA_PREFIX%\conda-bld\python
-conda-build -m ..\..\a\conda_build_config.yaml . --python 3.7 --no-build-id -c rdonnelly 2>&1 | C:\msys32\usr\bin\tee %CONDA_PREFIX%\conda-bld\python\build.log
-move %CONDA_PREFIX%\conda-bld\python %CONDA_PREFIX%\conda-bld\python-3.8-dbg
+conda-build -m ..\..\a\conda_build_config-dbg.yaml . --python 3.7 --no-build-id -c rdonnelly --keep-old-work --dirty 2>&1 | C:\msys32\usr\bin\tee %CONDA_PREFIX%\conda-bld\python\build.log
+move %CONDA_PREFIX%\conda-bld\python %CONDA_PREFIX%\conda-bld\python-3.8-dbg-%PF%
 
 set PY_INTERP_DEBUG=
 del /s /q %CONDA_PREFIX%\conda-bld\python
+del /s /q %CONDA_PREFIX%\conda-bld\python-3.8-%PF%
 mkdir %CONDA_PREFIX%\conda-bld\python
-conda-build -m ..\..\a\conda_build_config.yaml . --python 3.7 --no-build-id -c rdonnelly 2>&1 | C:\msys32\usr\bin\tee %CONDA_PREFIX%\conda-bld\python\build.log
-move %CONDA_PREFIX%\conda-bld\python %CONDA_PREFIX%\conda-bld\python-3.8
+conda-build -m ..\..\a\conda_build_config.yaml . --python 3.7 --no-build-id -c rdonnelly --keep-old-work --dirty 2>&1 | C:\msys32\usr\bin\tee %CONDA_PREFIX%\conda-bld\python\build.log
+move %CONDA_PREFIX%\conda-bld\python %CONDA_PREFIX%\conda-bld\python-3.8-%PF%

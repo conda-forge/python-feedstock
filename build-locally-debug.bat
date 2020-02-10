8b6b807779255d03dcd34f3a7b8f5cb79c8621c2
Ray Donnelly <mingw.android@gmail.com>
2020-02-10 23:01:38 +0100

    Not much

--- build-locally-debug.bat
+++ build-locally-debug.bat
@@ -21,25 +21,27 @@ call conda activate
 cd ..\..\..\r\c.wip\lief-feedstock
 
 set PY_INTERP_DEBUG=yes
+set CB_DEBUG=--debug
+set CB_DEBUG=
 call conda activate
 del /s /q %CONDA_PREFIX%\conda-bld\lief
 del /s /q %CONDA_PREFIX%\conda-bld\lief-3.8-dbg-%PF%
 mkdir %CONDA_PREFIX%\conda-bld\lief
-conda-build -m ..\..\a\conda_build_config-dbg.yaml . --python 3.8 --no-build-id -c rdonnelly --keep-old-work --dirty 2>&1 | C:\msys32\usr\bin\tee %CONDA_PREFIX%\conda-bld\lief\build.log
+conda-build -m ..\..\a\conda_build_config-dbg.yaml . --python 3.8 --no-build-id -c rdonnelly --keep-old-work --dirty %CB_DEBUG% 2>&1 | C:\msys32\usr\bin\tee %CONDA_PREFIX%\conda-bld\lief\build.log
 move %CONDA_PREFIX%\conda-bld\lief %CONDA_PREFIX%\conda-bld\lief-3.8-dbg-%PF%
 
 set PY_INTERP_DEBUG=
 del /s /q %CONDA_PREFIX%\conda-bld\lief
 del /s /q %CONDA_PREFIX%\conda-bld\lief-3.8-%PF%
 mkdir %CONDA_PREFIX%\conda-bld\lief
-conda-build -m ..\..\a\conda_build_config.yaml . --python 3.8 --no-build-id -c rdonnelly --keep-old-work --dirty 2>&1 | C:\msys32\usr\bin\tee %CONDA_PREFIX%\conda-bld\lief\build.log
+conda-build -m ..\..\a\conda_build_config.yaml . --python 3.8 --no-build-id -c rdonnelly --keep-old-work --dirty %CB_DEBUG% 2>&1 | C:\msys32\usr\bin\tee %CONDA_PREFIX%\conda-bld\lief\build.log
 move %CONDA_PREFIX%\conda-bld\lief %CONDA_PREFIX%\conda-bld\lief-3.8-%PF%
 
 set PY_INTERP_DEBUG=
 del /s /q %CONDA_PREFIX%\conda-bld\lief
 del /s /q %CONDA_PREFIX%\conda-bld\lief-3.7-%PF%
 mkdir %CONDA_PREFIX%\conda-bld\lief
-conda-build -m ..\..\a\conda_build_config.yaml . --python 3.7 --no-build-id -c rdonnelly --keep-old-work --dirty 2>&1 | C:\msys32\usr\bin\tee %CONDA_PREFIX%\conda-bld\lief\build.log
+conda-build -m ..\..\a\conda_build_config.yaml . --python 3.7 --no-build-id -c rdonnelly --keep-old-work --dirty %CB_DEBUG% 2>&1 | C:\msys32\usr\bin\tee %CONDA_PREFIX%\conda-bld\lief\build.log
 move %CONDA_PREFIX%\conda-bld\lief %CONDA_PREFIX%\conda-bld\lief-3.7-%PF%
 
 cd %OLDCD%

c59068de0b90dba2e2ea240b54f64c631df661a2
Ray Donnelly <mingw.android@gmail.com>
2020-02-15 17:30:58 +0000

    Fix final debug logging

--- build-locally-debug.bat
+++ build-locally-debug.bat
@@ -125,7 +125,7 @@ if %THREE_SEVEN%==yes (
     python -c "from ctypes import *; from sys import *; cdll.LoadLibrary(prefix+'/lib/site-packages/lief.pyd')" 2>&1 | C:\msys32\usr\bin\tee %CONDA_PREFIX%\debug-v1.log
   )
   if %RELEASE_ME%==yes (
-    conda activate %CB_CROOT%\lief-3.7.6-win-64\_h_env_moved_py-lief-0.10.1-py37ha925a31_0_win-64
+    conda activate %CB_CROOT%\lief-3.7.6-win-64\_h_env_moved_py-lief-0.10.1-py37ha4be599_0_win-64
     python -c "from ctypes import *; from sys import *; cdll.LoadLibrary(prefix+'/lib/site-packages/lief.pyd')" 2>&1 | C:\msys32\usr\bin\tee %CONDA_PREFIX%\debug-v1.log
   )
 )
@@ -138,17 +138,17 @@ if %THREE_EIGHT%==yes (
     python -c "from ctypes import *; from sys import *; cdll.LoadLibrary(prefix+'/lib/site-packages/lief.pyd')" 2>&1 | C:\msys32\usr\bin\tee %CONDA_PREFIX%\debug-v2.log
   )
   if %RELEASE_ME%==yes (
-    conda activate %CB_CROOT%\conda-bld\lief-3.8.1-win-64\_h_env
+    conda activate %CB_CROOT%\lief-3.8.1-win-64\_h_env
     python -c "from ctypes import *; from sys import *; cdll.LoadLibrary(prefix+'/lib/site-packages/lief.pyd')" 2>&1 | C:\msys32\usr\bin\tee %CONDA_PREFIX%\debug-v2.log
   )
 )
 if %THREE_SEVEN%==yes (
   if %DEBUG_ME%==yes (
-    conda activate %CB_CROOT%\conda-bld\lief-3.7.6-win-64\_h_env_moved_py-lief-0.10.1-py37ha925a31_0_win-64
+    conda activate %CB_CROOT%\lief-dbg-3.7.6-win-64\_h_env_moved_py-lief-0.10.1-py37h5824298_0_win-64
     python -c "from ctypes import *; from sys import *; cdll.LoadLibrary(prefix+'/lib/site-packages/lief.pyd')" 2>&1 | C:\msys32\usr\bin\tee %CONDA_PREFIX%\debug-v2.log
   )
   if %RELEASE_ME%==yes (
-    conda activate %CB_CROOT%\conda-bld\lief-dbg-3.7.6-win-64\_h_env_moved_py-lief-0.10.1-py37h5824298_0_win-64
+    conda activate %CB_CROOT%\lief-3.7.6-win-64\_h_env_moved_py-lief-0.10.1-py37ha4be599_0_win-64
     python -c "from ctypes import *; from sys import *; cdll.LoadLibrary(prefix+'/lib/site-packages/lief.pyd')" 2>&1 | C:\msys32\usr\bin\tee %CONDA_PREFIX%\debug-v2.log
   )
 )

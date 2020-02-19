102b6e8c0bd3d0a60cb36fd1bcc53f924e968da5
Ray Donnelly <mingw.android@gmail.com>
2020-02-19 23:47:50 +0100

    Another log fix

--- build-something-debug.bat
+++ build-something-debug.bat
@@ -17,7 +17,7 @@ if %_BS_THREE_EIGHT%==yes (
     if "%_BS_SKIP_WILDCARD%"=="" or not dir %_BS_CB_CROOT%\%_BS_SKIP_WILDCARD_PREFIX%_dbg.tar.bz2 /b /a-d >nul 2>&1 (
       set PY_INTERP_DEBUG=yes
       set _BS_BLD_DIRNAME=%_BS_NAME%-dbg-3.8.1-%_BS_CB_CONDA_SUBDIR%
-      set _BS_DST_DIR=%_BS_CB_CROOT%\!BLD_DIRNAME!
+      set _BS_DST_DIR=%_BS_CB_CROOT%\!_BS_BLD_DIRNAME!
       del /s /q !DST_DIR!
       mkdir !DST_DIR!
       conda-build --croot %_BS_CB_CROOT% --build-id-pat {n}-dbg-3.8.1-%PF% -m %DBG_CFG% . --python 3.8 %CHANNELS% %CB_KEEP% %CB_DEBUG% 2>&1 | C:\msys32\usr\bin\tee !DST_DIR!\build.log
@@ -28,7 +28,7 @@ if %_BS_THREE_EIGHT%==yes (
     if "%_BS_SKIP_WILDCARD%"=="" or not dir %_BS_CB_CROOT%\%_BS_SKIP_WILDCARD_PREFIX%.tar.bz2 /b /a-d >nul 2>&1 (
       set PY_INTERP_DEBUG=yes
       set _BS_BLD_DIRNAME=%_BS_NAME%-3.8.1-%_BS_CB_CONDA_SUBDIR%
-      set _BS_DST_DIR=%_BS_CB_CROOT%\!BLD_DIRNAME!
+      set _BS_DST_DIR=%_BS_CB_CROOT%\!_BS_BLD_DIRNAME!
       del /s /q !DST_DIR!
       mkdir !DST_DIR!
       conda-build --croot %_BS_CB_CROOT% --build-id-pat {n}-3.8.1-%PF% -m %DBG_CFG% . --python 3.8 %CHANNELS% %CB_KEEP% %CB_DEBUG% 2>&1 | C:\msys32\usr\bin\tee !DST_DIR!\build.log
@@ -41,7 +41,7 @@ if %_BS_THREE_SEVEN%==yes (
     if "%_BS_SKIP_WILDCARD%"=="" or not dir %_BS_CB_CROOT%\%_BS_SKIP_WILDCARD_PREFIX%_dbg.tar.bz2 /b /a-d >nul 2>&1 (
       set PY_INTERP_DEBUG=yes
       set _BS_BLD_DIRNAME=%_BS_NAME%-dbg-3.7.6-%_BS_CB_CONDA_SUBDIR%
-      set _BS_DST_DIR=%_BS_CB_CROOT%\!BLD_DIRNAME!
+      set _BS_DST_DIR=%_BS_CB_CROOT%\!_BS_BLD_DIRNAME!
       del /s /q !DST_DIR!
       mkdir !DST_DIR!
       conda-build --croot %_BS_CB_CROOT% --build-id-pat {n}-dbg-3.7.6-%PF% -m %DBG_CFG% . --python 3.7 %CHANNELS% %CB_KEEP% %CB_DEBUG% 2>&1 | C:\msys32\usr\bin\tee !DST_DIR!\build.log
@@ -52,7 +52,7 @@ if %_BS_THREE_SEVEN%==yes (
     if "%_BS_SKIP_WILDCARD%"=="" or not dir %_BS_CB_CROOT%\%_BS_SKIP_WILDCARD_PREFIX%.tar.bz2 /b /a-d >nul 2>&1 (
       set PY_INTERP_DEBUG=yes
       set _BS_BLD_DIRNAME=%_BS_NAME%-3.7.6-%_BS_CB_CONDA_SUBDIR%
-      set _BS_DST_DIR=%_BS_CB_CROOT%\!BLD_DIRNAME!
+      set _BS_DST_DIR=%_BS_CB_CROOT%\!_BS_BLD_DIRNAME!
       del /s /q !DST_DIR!
       mkdir !DST_DIR!
       conda-build --croot %_BS_CB_CROOT% --build-id-pat {n}-3.7.6-%PF% -m %DBG_CFG% . --python 3.7 %CHANNELS% %CB_KEEP% %CB_DEBUG% 2>&1 | C:\msys32\usr\bin\tee !DST_DIR!\build.log

85c02635b3e6111360a6ca1c5274e752b2165615
Ray Donnelly <mingw.android@gmail.com>
2020-03-06 19:33:03 +0100

    Start using binary_has_prefix_files, another test build script (conda virtual packages issue?)

--- build-against-conda-versions.sh
+++ build-against-conda-versions.sh
@@ -3,43 +3,53 @@
 # Only conda versions in which 'source /dev/start /opt/conda' works
 # can be used here.
 declare -a CONDA_VERSIONS=()
-CONDA_VERSIONS+=(us/3.8.1)
-CONDA_VERSIONS+=(us/3.8.2)
+CONDA_VERSIONS+=(us/4.8.1)
+# CONDA_VERSIONS+=(us/4.8.2)
 CONDA_VERSIONS+=(ma/master)
 
 declare -a CONDA_BUILD_VERSIONS=()
-CONDA_BUILD_VERSIONS+=(us/3.18.11)
+# CONDA_BUILD_VERSIONS+=(us/3.18.11)
 CONDA_BUILD_VERSIONS+=(us/master)
+
+ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 declare -a ARGS=()
 ARGS+=(--debug)
 
 conda activate
 
-mkdir /tmp/builds.py.c-cb
-pushd /tmp/builds.py.c-cb
+ROOT=/opt/builds-py.c-cb
+
+# rm -rf $ROOT
+mkdir $ROOT
+pushd $ROOT
 
   for CONDA_VERSION in "${CONDA_VERSIONS[@]}"; do
-    C_DN=${CONDA_VERSION//_/}
+    C_DN=${CONDA_VERSION//\//\_}
+    C_TAG=${CONDA_VERSION/#*\/}
+    C_TAG=${C_TAG##\/}
     echo "C_DN=${C_DN}"
+    echo "C_TAG=${C_TAG}"
     git clone https://github.com/mingwandroid/conda.git -o ma conda.${C_DN}
     pushd conda.${C_DN}
       git remote add us https://github.com/conda/conda.git
       git fetch us
-      git checkout ${C_DN}
+      git checkout ${C_TAG}
+      rm -f conda.egg-info
       source dev/start /opt/conda
     popd
     for CONDA_BUILD_VERSION in "${CONDA_BUILD_VERSIONS[@]}"; do
-      CB_DN=${CONDA_BUILD_VERSION//_/}
+      CB_DN=${CONDA_BUILD_VERSION//\//\_}
+      CB_TAG=${CONDA_BUILD_VERSION/#*\/}
       echo "DN=${DN}"
       git clone https://github.com/mingwandroid/conda-build.git -o ma conda-build.${CB_DN}
-      pushd conda.${CB_DN}
+      pushd conda-build.${CB_DN}
         git remote add us https://github.com/conda/conda-build.git
         git fetch us
-        git checkout ${CB_DN}
+        git checkout ${CB_TAG}
         python setup.py develop
       popd
-      conda-build $(dirname ${BASH_SOURCE[0]}) "${ARGS[@]}" --croot ${PWD} 2>&1 | tee ${PWD}/python.${C_DN}.${CB_DN}
+      conda-build ${ABSOLUTE_PATH}/recipe -m /opt/Shared.local/r/a/conda_build_config.yaml "${ARGS[@]}" --croot ${PWD} --no-build-id 2>&1 | tee ${PWD}/python.${C_DN}.${CB_DN}.log
     done
   done
 

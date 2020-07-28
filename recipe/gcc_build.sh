da4a889b12be883eba40cfb27f53e4526f29b33f
Matthew R Becker <beckermr@users.noreply.github.com>
2020-07-28 10:14:21 -0500

    Update gcc_build.sh

--- recipe/gcc_build.sh
+++ recipe/gcc_build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [[ ${HOST} =~ .*darwin.* ]]; then
-  :
-else
-  export BUILD=${HOST}
-fi
-
 # The LTO/PGO information was sourced from @pitrou and the Debian rules file in:
 # http://http.debian.net/debian/pool/main/p/python3.6/python3.6_3.6.2-2.debian.tar.xz
 # https://packages.debian.org/source/sid/python3.6

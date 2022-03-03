e4b7a457000235d88cd0212c3c99e068e5be4477
Kai Tietz <ktietz@anaconda.com>
2022-03-03 10:46:35 +0100

    avoid removal of spaces instead of CONDA_BUILD_SYSROOT

--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -450,8 +450,11 @@ pushd "${PREFIX}"/lib/python${VER}
   # Append the conda-forge zoneinfo to the end
   sed -i.bak "s@zoneinfo'@zoneinfo:$PREFIX/share/tzinfo'@g" sysconfigfile
   # Remove osx sysroot as it depends on the build machine
-  sed -i.bak "s@-isysroot @@g" sysconfigfile
-  sed -i.bak "s@$CONDA_BUILD_SYSROOT @@g" sysconfigfile
+  # be sure CONDA_BUILD_SYSROOT has value, as other we will remove here instead spaces
+  if [[ ${HOST} =~ .*darwin.* ]] && [[ -n ${CONDA_BUILD_SYSROOT} ]]; then
+    sed -i.bak "s@-isysroot @@g" sysconfigfile
+    sed -i.bak "s@$CONDA_BUILD_SYSROOT @@g" sysconfigfile
+  fi
   # Remove unfilled config option
   sed -i.bak "s/@SGI_ABI@//g" sysconfigfile
   sed -i.bak "s@$BUILD_PREFIX/bin/${HOST}-llvm-ar@${HOST}-ar@g" sysconfigfile

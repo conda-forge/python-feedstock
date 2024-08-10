a1b90efad4cb6f58c697a22923ece9b05f37219f
Isuru Fernando <isuruf@gmail.com>
2024-08-10 13:08:50 -0500

    Expand comment

--- recipe/sitecustomize.py
+++ recipe/sitecustomize.py
@@ -2,6 +2,16 @@ import site, sys, os
 
 dirs_to_add = []
 # Workaround for https://github.com/conda/conda/issues/14053
+# Older conda versions install noarch: python packages in wrong places.
+# For example python3.1 because older conda assumed python minor version
+# will have only one digit. noarhc pkgs for freethreading builds are supposed
+# to be installed into <prefix>/lib/python3.13t/site-packages, but conda
+# installs them to <prefix>/lib/python3.13/site-packages.
+# The workaround is to add all these wrong paths to sys.path using
+# site.addsitedir so that cpython and other tools like pip know about these
+# locations to check when importing packages and uninstalling packages.
+# When installing packages, pip will use the correct location
+# <prefix>/lib/python3.13t/site-packages.
 if 't' in sys.abiflags:
     dirs_to_add.append(os.path.join(sys.prefix, 'lib', f'python3.{sys.version_info[1]}', 'site-packages'))
 # Workaround for https://github.com/conda/conda/issues/10969

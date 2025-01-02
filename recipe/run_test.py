7b2895bd69ad4189e0ce8f1e47610e44156bf7b0
Mark Harfouche <mark.harfouche@gmail.com>
2025-01-01 19:57:32 -0500

    Super slim python without tk, readline and sqlite

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -47,7 +47,7 @@ import _multibytecodec
 import _multiprocessing
 import _random
 import _socket
-import _sqlite3
+# import _sqlite3
 import _ssl
 import _struct
 import _testcapi
@@ -80,7 +80,7 @@ if sys.platform != 'win32':
         import _curses_panel
     import fcntl
     import grp
-    import readline
+    # import readline
     import resource
     import syslog
     import termios
@@ -90,14 +90,14 @@ if os.getenv('PY_INTERP_DEBUG') == 'yes':
         assert 'd' in sys.abiflags
     assert 'gettotalrefcount' in dir(sys)
 
-if not (armv6l or armv7l or ppc64le or osx105):
-    import tkinter
-    import turtle
-    import _tkinter
-    print('TK_VERSION: %s' % _tkinter.TK_VERSION)
-    print('TCL_VERSION: %s' % _tkinter.TCL_VERSION)
-    TCLTK_VER = os.getenv('tk')
-    assert _tkinter.TK_VERSION == _tkinter.TCL_VERSION == TCLTK_VER
+# if not (armv6l or armv7l or ppc64le or osx105):
+#     import tkinter
+#     import turtle
+#     import _tkinter
+#     print('TK_VERSION: %s' % _tkinter.TK_VERSION)
+#     print('TCL_VERSION: %s' % _tkinter.TCL_VERSION)
+#     TCLTK_VER = os.getenv('tk')
+#     assert _tkinter.TK_VERSION == _tkinter.TCL_VERSION == TCLTK_VER
 
 import ssl
 print('OPENSSL_VERSION:', ssl.OPENSSL_VERSION)

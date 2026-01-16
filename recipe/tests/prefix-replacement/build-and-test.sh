b422d66386d709cf04d54e4dd49cb3a435565239
Michał Górny <mgorny@quansight.com>
2026-01-16 17:42:42 +0100

    Fix the linking test case
    Now python3-config defaults to shared linking, so perform that part of
    the test first.  Then remove the shared libraries to force static
    linking.
    
    Signed-off-by: Michał Górny <mgorny@quansight.com>

--- recipe/tests/prefix-replacement/build-and-test.sh
+++ recipe/tests/prefix-replacement/build-and-test.sh
@@ -2,32 +2,7 @@
 
 set -ex
 
-if [[ "$PKG_NAME" == "libpython-static" ]]; then
-  # see bpo44182 for why -L${CONDA_PREFIX}/lib is added
-  ${CC} a.c $(python3-config --cflags) $(python3-config --embed --ldflags) -L${CONDA_PREFIX}/lib -o ${CONDA_PREFIX}/bin/embedded-python-static
-  if [[ "$target_platform" == linux-* ]]; then
-    if ${READELF} -d ${CONDA_PREFIX}/bin/embedded-python-static | rg libpython; then
-      echo "ERROR :: Embedded python linked to shared python library. It is expected to link to the static library."
-    fi
-  elif [[ "$target_platform" == osx-* ]]; then
-    if ${OTOOL} -l ${CONDA_PREFIX}/bin/embedded-python-static | rg libpython; then
-      echo "ERROR :: Embedded python linked to shared python library. It is expected to link to the static library."
-    fi
-  fi
-  ${CONDA_PREFIX}/bin/embedded-python-static
-
-  # I thought this would prefer the shared library for Python. I was wrong:
-  # EMBED_LDFLAGS=$(python3-config --ldflags)
-  # re='^(.*)(-lpython[^ ]*)(.*)$'
-  # if [[ ${EMBED_LDFLAGS} =~ $re ]]; then
-  #   EMBED_LDFLAGS="${BASH_REMATCH[1]} ${BASH_REMATCH[3]} -Wl,-Bdynamic ${BASH_REMATCH[2]}"
-  # fi
-  # ${CC} a.c $(python3-config --cflags) ${EMBED_LDFLAGS} -o ${CONDA_PREFIX}/bin/embedded-python-shared
-
-  # Brute-force way of linking to the shared library, sorry!
-  rm -rf ${CONDA_PREFIX}/lib/libpython*.a
-fi
-
+# see bpo44182 for why -L${CONDA_PREFIX}/lib is added
 ${CC} a.c $(python3-config --cflags) \
     $(python3-config --embed --ldflags) \
     -L${CONDA_PREFIX}/lib -Wl,-rpath,${CONDA_PREFIX}/lib \
@@ -43,5 +18,25 @@ elif [[ "$target_platform" == osx-* ]]; then
   fi
 fi
 ${CONDA_PREFIX}/bin/embedded-python-shared
+
+if [[ "$PKG_NAME" == "libpython-static" ]]; then
+  # remove the shared library to force static linking
+  rm -f ${CONDA_PREFIX}/lib/libpython*.{so,dylib}
+
+  ${CC} a.c $(python3-config --cflags) \
+      $(python3-config --embed --ldflags) \
+      -L${CONDA_PREFIX}/lib -Wl,-rpath,${CONDA_PREFIX}/lib \
+      -o ${CONDA_PREFIX}/bin/embedded-python-static
+  if [[ "$target_platform" == linux-* ]]; then
+    if ${READELF} -d ${CONDA_PREFIX}/bin/embedded-python-static | rg libpython; then
+      echo "ERROR :: Embedded python linked to shared python library. It is expected to link to the static library."
+    fi
+  elif [[ "$target_platform" == osx-* ]]; then
+    if ${OTOOL} -l ${CONDA_PREFIX}/bin/embedded-python-static | rg libpython; then
+      echo "ERROR :: Embedded python linked to shared python library. It is expected to link to the static library."
+    fi
+  fi
+  ${CONDA_PREFIX}/bin/embedded-python-static
+fi
 
 set +x

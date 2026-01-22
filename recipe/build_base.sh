051a5aea786ac33332f48bd3888865a7c25d9510
Michał Górny <mgorny@quansight.com>
2026-01-22 15:59:51 +0100

    Split shared Python library to `libpython`
    Fixes #843
    
    Signed-off-by: Michał Górny <mgorny@quansight.com>
    Co-Authored-By: H. Vetinari <h.vetinari@gmx.com>

--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -365,13 +365,6 @@ if [[ ${_OPTIMIZED} == yes ]]; then
     _FLAGS_REPLACE+=("")
   done
 fi
-# Install the shared library (for people who embed Python only, e.g. GDB).
-# Linking module extensions to this on Linux is redundant (but harmless).
-# Linking module extensions to this on Darwin is harmful (multiply defined symbols).
-cp -pf ${_buildd_shared}/libpython*${SHLIB_EXT}* ${PREFIX}/lib/
-if [[ ${target_platform} =~ .*linux.* ]]; then
-  ln -sf ${PREFIX}/lib/libpython${VERABI}${SHLIB_EXT}.1.0 ${PREFIX}/lib/libpython${VERABI}${SHLIB_EXT}
-fi
 
 SYSCONFIG=$(find ${_buildd_static}/$(cat ${_buildd_static}/pybuilddir.txt) -name "_sysconfigdata*.py" -print0)
 cat ${SYSCONFIG} | ${SYS_PYTHON} "${RECIPE_DIR}"/replace-word-pairs.py \
@@ -496,7 +489,6 @@ rm ${PREFIX}/lib/libpython${VERABI}.a
 if [[ ${PY_INTERP_DEBUG} == yes ]]; then
   rm ${PREFIX}/bin/python${VER}
   ln -s ${PREFIX}/bin/python${VERABI} ${PREFIX}/bin/python${VER}
-  ln -s ${PREFIX}/lib/libpython${VERABI}.so ${PREFIX}/lib/libpython${VER}.so
   ln -s ${PREFIX}/include/python${VERABI} ${PREFIX}/include/python${VER}
 fi
 
